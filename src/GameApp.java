dc3b777d75d292f8e267b83cd3f731a4ef3fcb66
Colin M <colintmcneill@gmail.com>
2019-11-03 00:13:46 -0400

    first working iteration

--- src/GameApp.java
+++ src/GameApp.java
@@ -1,2 +1,15 @@
-public class GameApp {
+import javax.swing.*;
+
+public class GameApp extends JFrame {
+    public GameApp(String title) {
+        super(title);
+        this.setSize(600,700);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(new GamePanel());
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        GameApp app = new GameApp("Tic-Tac-Toe");
+    }
 }
---
+++ src/GamePanel.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GamePanel extends JPanel {
+    public GamePanel() {
+        super();
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.setLayout(new GridLayout(3, 2, 3, 3));
+        for (int i = 0; i < 9; i++) {
+            JLabel cell = new JLabel("");
+            cell.setBackground(Color.WHITE);
+            cell.setOpaque(true);
+            cell.setHorizontalAlignment(JLabel.CENTER);
+            cell.setVerticalAlignment(JLabel.BOTTOM);
+            cell.setFont(new Font("Sans Serif", Font.BOLD, 200));
+            cell.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (SwingUtilities.isRightMouseButton(e)) {
+                        cell.setText("o");
+                    }
+                    if (SwingUtilities.isLeftMouseButton(e)) {
+                        cell.setText("x");
+                    }
+                }
+            });
+            this.add(cell);
+        }
+    }
+
+}
